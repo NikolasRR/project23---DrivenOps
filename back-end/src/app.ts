3898f90db60a34a975ea17263ae6e55d6a222852
Nikolas <nkls99@gmail.com>
2022-08-21 20:42:22 -0300

    12312

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -20,7 +20,7 @@ app.post("/students", async (req: Request, res: Response) => {
 
   res.sendStatus(201); // created
 });
-//para fazer novo commit 3
+//para fazer novo commit 323
 app.get("/students/random", async (req: Request, res: Response) => {
   const students = await prisma.student.findMany();
   if (students.length > 0) {
